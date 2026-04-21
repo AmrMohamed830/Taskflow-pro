05ec302eb3efa60315fc58a355a92ad0116c7502
Amr ElToukhy <amr2553473@gmail.com>
2026-04-21 18:42:27 +0200

    build CTA section ui with signup prompt

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
+import CTASection from "@/components/home/CTASection";
 import FeaturesSection from "@/components/home/featuresSection";
 import Hero from "@/components/home/hero";
 import Navbar from "@/components/home/navbar";
 
 export default function Home() {
   return (
-<div className=""> 
+<> 
 <Navbar/>
 <Hero/>
 <FeaturesSection/>
-</div>
+<CTASection/>
+</>
 
   );
 }
---
+++ components/home/CTASection.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Button } from "../ui/button";
+import { ArrowRight } from "lucide-react";
+
+const CTASection = () => {
+    return (
+        <section className="py-25 border-b-1">
+            <div className="max-w-7xl mx-auto px-4">
+                <div className="flex flex-col items-center gap-6 bg-secondary-brand py-13 rounded-2xl">
+                    <h2 className="font-bold text-[40px]">Ready to get started?</h2>
+                    <p className="text-gray-brand">
+                        Join thousands of teams already using TaskFlow to boost
+                        their productivity.
+                    </p>
+                    <Button variant="taskflow" size="lg" className="gap-5">
+                        Create Free Account
+                        <ArrowRight />
+                    </Button>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default CTASection;
